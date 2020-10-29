ead4c28b782411bae3dc98aaebd563b4f780057b
Eric Suen <eric.suen@HK-CE-LTM-20016.local>
2020-10-29 20:02:25 +0800

    change deploy.sh a bit

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com-random-doodler:random-doodler/peter-and-daisy.git master:gh-pages
+git push -f git@github.com:webberpuma/peter-and-daisy.git master:gh-pages
 
 cd -
